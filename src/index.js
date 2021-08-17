e628185b598fc36e1a6ae02082919d1c6178f98f
Jane Yushchenko <kelenella@ukr.net>
2021-08-17 17:55:30 +0300

    add template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import menuData from './menu.json'
+import cardsGallery from './templates/cardsGallery.hbs'
+
+const list = document.querySelector('.js-menu');
+console.log(list);
+const markupCards = cardsGallery(menuData);
+console.log(markupCards);
