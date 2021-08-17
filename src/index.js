70b7c77fa2a100111c3db8e5fe0d05901bc6774c
Jane Yushchenko <kelenella@ukr.net>
2021-08-17 20:28:59 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import menuData from './menu.json'
 import cardsGallery from './templates/cardsGallery.hbs'
 
 const list = document.querySelector('.js-menu');
-console.log(list);
+
 const markupCards = cardsGallery(menuData);
-console.log(markupCards);
+
+list.insertAdjacentHTML('beforeend', markupCards)
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const sliderSwitch = document.querySelector("#theme-switch-toggle");
+const body = document.querySelector("body");
+sliderSwitch.addEventListener("change", checkColor);
+sliderSwitch.addEventListener("change", inLocaLeStorage);
+
+function checkColor() {
+  const check = sliderSwitch.checked;
+
+  if (check) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+  }
+}
+
+function inLocaLeStorage() {
+  const check = sliderSwitch.checked;
+
+  if (check) {
+    localStorage.setItem("theme", Theme.DARK);
+  } else {
+    localStorage.removeItem("theme");
+    localStorage.setItem("theme", Theme.LIGHT);
+  }
+}
+
+const changeLocalTheme = localStorage.getItem("theme");
+if (changeLocalTheme === Theme.DARK) {
+  body.classList.add(Theme.DARK);
+  sliderSwitch.checked = true;
+}
